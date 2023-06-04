904cb0de359506e0ce79e7c684a1a887c6ae7c1f
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-06-04 18:59:01 +0300

    enable web security

---
+++ src/main/java/com/ivan/web4back/model/user/AuthProvider.java
@@ -0,0 +1,7 @@
+package com.ivan.web4back.model.user;
+
+public enum AuthProvider {
+    LOCAL,
+    GOOGLE,
+    VK;
+}
---
+++ src/main/java/com/ivan/web4back/model/user/UserEntity.java
@@ -0,0 +1,20 @@
+package com.ivan.web4back.model.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
+@Table(name = "users")
+public class UserEntity {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Enumerated(EnumType.STRING)
+    private AuthProvider provider;
+}
