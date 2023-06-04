4561d195f37f096c5cfc49b3997be606fe6ed63d
Ivan Kustarev <ivan.kustarev@tune-it.ru>
2023-06-04 16:35:31 +0300

    init

---
+++ src/main/java/com/ivan/web4back/Web4BackApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.web4back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Web4BackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Web4BackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/ivan/web4back/Web4BackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.web4back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Web4BackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
